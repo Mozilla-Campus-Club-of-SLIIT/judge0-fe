ee3af673a38561614ec40849b70dcb8c39bd94a5
Kavindu Nirmal <65773645+KavinduNirmal@users.noreply.github.com>
2026-03-25 20:53:44 +0530

    refactor: Hotfix 1-moving navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import HeroSection from '@/components/heroSection';
+import Navbar from '@/components/navbar/Navbar';
 
 export default function Home() {
-  return <HeroSection />;
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
+  );
 }
--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -1,38 +1,34 @@
 import Image from 'next/image';
-import Navbar from './navbar/Navbar';
 
 export default function heroSection() {
   return (
-    <>
-      <Navbar />
-      <div className="flex flex-col items-center justify-center p-6">
-        <h1 className="text-[20px] text-[#40fd51] pr-10 pl-10 p-0.5 relative ">
-          STATUS GREEN. WELCOME PARTICIPANTS.
-        </h1>
+    <div className="flex flex-col items-center justify-center p-6">
+      <h1 className="text-[20px] text-[#40fd51] pr-10 pl-10 p-0.5 relative ">
+        STATUS GREEN. WELCOME PARTICIPANTS.
+      </h1>
 
-        <Image
-          src="/assets/logo.svg"
-          width={550}
-          height={370}
-          className="w-137.5 h-92.5 relative "
-          alt="logo"
-        />
+      <Image
+        src="/assets/logo.svg"
+        width={550}
+        height={370}
+        className="w-137.5 h-92.5 relative "
+        alt="logo"
+      />
 
-        <div className="text-center text-3xl text-[#ededed] ">
-          <p>The ultimate arena for developers.</p>
+      <div className="text-center text-3xl text-[#ededed] ">
+        <p>The ultimate arena for developers.</p>
 
-          <p>
-            Test your skills, solve complex algorithmic challenges, and climb
-            the leaderboard.
-          </p>
-        </div>
-
-        <div>
-          <button className="text-[23px] border-2 cursor-pointer relative top-10 text-[#40fd51] pr-20 pl-20">
-            VIEW LEADERBOARD
-          </button>
-        </div>
+        <p>
+          Test your skills, solve complex algorithmic challenges, and climb the
+          leaderboard.
+        </p>
       </div>
-    </>
+
+      <div>
+        <button className="text-[23px] border-2 cursor-pointer relative top-10 text-[#40fd51] pr-20 pl-20">
+          VIEW LEADERBOARD
+        </button>
+      </div>
+    </div>
   );
 }
