ad9ff604d1490b03333bfbbffbf90d1e46b3e0ad
IT24104152 <gayuthdemika@gmail.com>
2026-04-01 22:46:40 +0530

    fix: hero section changes - reduced the text size - Gave some breathing space before and after button - Refactored the button into a reusable component to ensure consistent styling across the platform

---
+++ src/components/ViewLeaderboardButton/Button.tsx
@@ -0,0 +1,22 @@
+// components/Button/Button.tsx
+import React from 'react';
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const baseStyle =
+  'text-[23px] border-2 cursor-pointer relative top-10 text-[#40fd51] pr-20 pl-20 mt-1';
+
+export default function Button({
+  children,
+  className = '',
+  ...props
+}: ButtonProps) {
+  return (
+    <button {...props} className={`${baseStyle} ${className}`}>
+      {children}
+    </button>
+  );
+}
