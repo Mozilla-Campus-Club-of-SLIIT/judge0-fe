da7f9915688f573e928157a657174ff10682b2f8
Heshan Thenura Kariyawasam <75155192+heshanthenura@users.noreply.github.com>
2026-04-06 14:25:24 +0530

    chore: change languages to support server
    Removed outdated programming languages and versions from the list.

--- src/lib/langauges.ts
+++ src/lib/langauges.ts
@@ -12,28 +12,12 @@ export const languages = [
     name: 'Basic (FBC 1.07.1)',
   },
   {
-    id: 104,
-    name: 'C (Clang 18.1.8)',
-  },
-  {
-    id: 110,
-    name: 'C (Clang 19.1.7)',
-  },
-  {
     id: 75,
     name: 'C (Clang 7.0.1)',
   },
   {
     id: 76,
     name: 'C++ (Clang 7.0.1)',
-  },
-  {
-    id: 103,
-    name: 'C (GCC 14.1.0)',
-  },
-  {
-    id: 105,
-    name: 'C++ (GCC 14.1.0)',
   },
   {
     id: 48,
@@ -76,10 +60,6 @@ export const languages = [
     name: 'Common Lisp (SBCL 2.0.0)',
   },
   {
-    id: 90,
-    name: 'Dart (2.19.2)',
-  },
-  {
     id: 56,
     name: 'D (DMD 2.089.1)',
   },
@@ -108,32 +88,12 @@ export const languages = [
     name: 'Go (1.13.5)',
   },
   {
-    id: 95,
-    name: 'Go (1.18.5)',
-  },
-  {
-    id: 106,
-    name: 'Go (1.22.0)',
-  },
-  {
-    id: 107,
-    name: 'Go (1.23.5)',
-  },
-  {
     id: 88,
     name: 'Groovy (3.0.3)',
   },
   {
     id: 61,
     name: 'Haskell (GHC 8.8.1)',
-  },
-  {
-    id: 96,
-    name: 'JavaFX (JDK 17.0.6, OpenJFX 22.0.2)',
-  },
-  {
-    id: 91,
-    name: 'Java (JDK 17.0.6)',
   },
   {
     id: 62,
@@ -144,24 +104,8 @@ export const languages = [
     name: 'JavaScript (Node.js 12.14.0)',
   },
   {
-    id: 93,
-    name: 'JavaScript (Node.js 18.15.0)',
-  },
-  {
-    id: 97,
-    name: 'JavaScript (Node.js 20.17.0)',
-  },
-  {
-    id: 102,
-    name: 'JavaScript (Node.js 22.08.0)',
-  },
-  {
     id: 78,
     name: 'Kotlin (1.3.70)',
-  },
-  {
-    id: 111,
-    name: 'Kotlin (2.1.10)',
   },
   {
     id: 64,
@@ -196,10 +140,6 @@ export const languages = [
     name: 'PHP (7.4.1)',
   },
   {
-    id: 98,
-    name: 'PHP (8.3.11)',
-  },
-  {
     id: 43,
     name: 'Plain Text',
   },
@@ -212,32 +152,12 @@ export const languages = [
     name: 'Python (2.7.17)',
   },
   {
-    id: 92,
-    name: 'Python (3.11.2)',
-  },
-  {
-    id: 100,
-    name: 'Python (3.12.5)',
-  },
-  {
-    id: 109,
-    name: 'Python (3.13.2)',
-  },
-  {
-    id: 113,
-    name: 'Python (3.14.0)',
-  },
-  {
     id: 71,
     name: 'Python (3.8.1)',
   },
   {
     id: 80,
     name: 'R (4.0.0)',
-  },
-  {
-    id: 99,
-    name: 'R (4.4.1)',
   },
   {
     id: 72,
@@ -248,16 +168,8 @@ export const languages = [
     name: 'Rust (1.40.0)',
   },
   {
-    id: 108,
-    name: 'Rust (1.85.0)',
-  },
-  {
     id: 81,
     name: 'Scala (2.13.2)',
-  },
-  {
-    id: 112,
-    name: 'Scala (3.4.2)',
   },
   {
     id: 82,
@@ -270,14 +182,6 @@ export const languages = [
   {
     id: 74,
     name: 'TypeScript (3.7.4)',
-  },
-  {
-    id: 94,
-    name: 'TypeScript (5.0.3)',
-  },
-  {
-    id: 101,
-    name: 'TypeScript (5.6.2)',
   },
   {
     id: 84,
