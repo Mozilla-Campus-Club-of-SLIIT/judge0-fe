198d038a3263b8863580f65bf809862ac98d85f2
Seniru Pasan Indira <senirupasan@gmail.com>
2026-04-06 20:29:39 +0530

    feat: register with accounts (#93)
    * feat: register with accounts
    
    * chore: add login with sliitmozilla
    
    ---------
    
    Co-authored-by: Heshan Thenura Kariyawasam <heshanthenura@protonmail.com>

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,9 +3,16 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { NAV_LINKS } from '@/lib/navbar';
 import { useAuth } from '@/context/AuthContext';
+import { useEffect, useState } from 'react';
 
 export default function Navbar() {
   const { user } = useAuth();
+  const [origin, setOrigin] = useState('');
+
+  useEffect(() => {
+    setOrigin(window.location.origin);
+  }, []);
+
   return (
     <nav className="relative z-10 flex items-center justify-between px-8 py-4 pb-1 bg-transparent">
       {/* Logo */}
@@ -43,12 +50,37 @@ export default function Navbar() {
           LOGOUT
         </Link>
       ) : (
-        <Link
-          href="/login"
-          className="hidden md:inline-flex items-center px-10 py-1.5 text-sm font-semibold tracking-wider text-[#40FD51] border border-[#40FD51] cursor-pointer transition-colors duration-200 hover:bg-[#40FD51] hover:text-black"
-        >
-          LOGIN
-        </Link>
+        <div>
+          {/* <Link
+            href="/login"
+            className="hidden md:inline-flex items-center px-10 py-1.5 text-sm font-semibold tracking-wider text-[#40FD51] border border-[#40FD51] cursor-pointer transition-colors duration-200 hover:bg-[#40FD51] hover:text-black"
+          >
+            LOGIN
+          </Link>
+          <a
+            href={
+              'https://accounts.sliitmozilla.org/api/authorize?redirect=' +
+              encodeURIComponent(origin + '/callback')
+            }
+          >
+            Login with SLIIT Mozilla
+          </a> */}
+          <button>
+            <a
+              href={
+                'https://accounts.sliitmozilla.org/api/authorize?redirect=' +
+                encodeURIComponent(origin + '/callback')
+              }
+            >
+              <Image
+                src="/login_logo.svg"
+                alt="SLIIT Mozilla Logo"
+                width={200}
+                height={200}
+              />
+            </a>
+          </button>
+        </div>
       )}
     </nav>
   );
