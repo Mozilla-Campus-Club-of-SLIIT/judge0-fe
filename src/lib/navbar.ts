df02d0ded694e537ac9a7db873ba1b606494c060
Heshan Thenura Kariyawasam <heshanthenura@protonmail.com>
2026-04-08 10:07:40 +0530

    chore: add about to navbar

--- src/lib/navbar.ts
+++ src/lib/navbar.ts
@@ -8,5 +8,5 @@ export const NAV_LINKS: NavLink[] = [
   { label: 'Home', href: '/', secure: false },
   { label: 'Challenges', href: '/challenges', secure: true },
   { label: 'Leaderboard', href: '/leaderboard', secure: false },
-  // { label: 'Profile', href: '/profile', secure: true },
+  { label: 'About', href: '/about', secure: false },
 ];
